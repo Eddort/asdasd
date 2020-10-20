38e0b2e0b87600e8b51285efc1e0807ce09f4fce
Alexander <webisforce@gmail.com>
2020-10-20 13:59:14 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f589c347e1a0a001e0ca4d4",
+				"5f589c347e1a0a001e0ca4d6"
+			]
+		},
+		"5f589c347e1a0a001e0ca4d4": {
+			"id": "5f589c347e1a0a001e0ca4d4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f589c347e1a0a001e0ca4d6": {
+			"id": "5f589c347e1a0a001e0ca4d6",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
